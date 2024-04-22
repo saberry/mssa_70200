f4c595d08249ce49e0f726553928d2df38e3c92a
Seth Berry <seth.berry@nd.edu>
2024-04-22 07:30:50 -0400

    Complete pbp scrape

--- code/nba_play_by_play_scrape_rows.py
+++ code/nba_play_by_play_scrape_rows.py
@@ -14,70 +14,68 @@ chrome = pw.chromium.launch(headless=True)
 
 page = chrome.new_page()
 
-for link in pbp_links['game_pbp_links'].loc[1]:
-  try:
-    page.goto(link)
+for link in pbp_links['game_pbp_links']:
+    try:
+        page.goto(link)
 
-    quarter_button_count = page.locator("css=.Button--unstyled.tabs__link").count()
+        quarter_button_count = page.locator("css=.Button--unstyled.tabs__link").count()
 
-    quarter_tables = []
+        quarter_tables = []
 
-    for quarter in range(quarter_button_count):
-      page.locator("css=.Button--unstyled.tabs__link").nth(quarter).click()
-      
-      row_count = page.locator("css=.playByPlay__tableRow").count()
-      
-      period_results = []
-      
-      period = quarter + 1
-      
-      for row in range(row_count):
-        
-        time = page.locator(
-          "css=.playByPlay__tableRow"
-          ).nth(row).locator('css=.playByPlay__time.Table__TD').inner_html()
-          
-        logo = page.locator(
-          "css=.playByPlay__tableRow"
-          ).nth(row).locator('css=.playByPlay__logo.Table__TD').inner_html()  
-        
-        play = page.locator(
-          "css=.playByPlay__tableRow"
-          ).nth(row).locator('css=.playByPlay__text.tl.Table__TD').inner_html()
-        
-        away_score = page.locator(
-          "css=.playByPlay__tableRow"
-          ).nth(row).locator('css=.playByPlay__score.playByPlay__score--away.tr.Table__TD').inner_html()
-        
-        home_score = page.locator(
-          "css=.playByPlay__tableRow"
-          ).nth(row).locator('css=.playByPlay__score.playByPlay__score--home.tr.Table__TD').inner_html()
-          
-        output = pd.DataFrame(
-          {'time': [time], 'logo': [logo], 'play': [play], 
-          'away_score': [away_score], 'home_score': [home_score], 'period': [period]}
-          )  
-          
-        period_results.append(output)
-        
-      period_results = pd.concat(period_results)
-      
-      quarter_tables.append(period_results)
-      
-      quarter_tables = pd.concat(quarter_tables)
+        for quarter in range(quarter_button_count):
+            page.locator("css=.Button--unstyled.tabs__link").nth(quarter).click()
 
-    results.append(quarter_tables)
-    
-    # print('success')
-    
-  except:   
-    print("fail")
-    results.append(pd.DataFrame())
+            row_count = page.locator("css=.playByPlay__tableRow").count()
 
+            period_results = []
 
+            period = quarter + 1
 
+            for row in range(row_count):
+                time = page.locator(
+                    "css=.playByPlay__tableRow"
+                ).nth(row).locator('css=.playByPlay__time.Table__TD').inner_html()
+
+                logo = page.locator(
+                    "css=.playByPlay__tableRow"
+                ).nth(row).locator('css=.playByPlay__logo.Table__TD').inner_html()
+
+                play = page.locator(
+                    "css=.playByPlay__tableRow"
+                ).nth(row).locator('css=.playByPlay__text.tl.Table__TD').inner_html()
+
+                away_score = page.locator(
+                    "css=.playByPlay__tableRow"
+                ).nth(row).locator('css=.playByPlay__score.playByPlay__score--away.tr.Table__TD').inner_html()
+
+                home_score = page.locator(
+                    "css=.playByPlay__tableRow"
+                ).nth(row).locator('css=.playByPlay__score.playByPlay__score--home.tr.Table__TD').inner_html()
+
+                output = pd.DataFrame(
+                    {'time': [time], 'logo': [logo], 'play': [play],
+                    'away_score': [away_score], 'home_score': [home_score], 'period': [period]}
+                )
+
+                period_results.append(output)
+
+            period_results = pd.concat(period_results)
+
+            quarter_tables.append(period_results)
+
+        quarter_tables = pd.concat(quarter_tables)
+
+        results.append(quarter_tables)
+
+        # print('success')
+
+    except Exception as e:
+        print("An error occurred:", e)
+        results.append(pd.DataFrame())
 
 results = pd.concat(results)
+
+results.to_csv("all_nba_pbp_data.csv", index = False)
 
 chrome.close()
 
